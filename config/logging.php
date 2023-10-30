22a4caffc25b547a992aa1544af7dd3b824a1183
taimanna <nhannt.gm@gmail.com>
2023-10-30 12:24:45 +0700

    test push

--- config/logging.php
+++ config/logging.php
@@ -27,7 +27,7 @@ return [
     | This option controls the log channel that should be used to log warnings
     | regarding deprecated PHP and library features. This allows you to get
     | your application ready for upcoming major versions of dependencies.
-    |
+    | tripu
     */
 
     'deprecations' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
