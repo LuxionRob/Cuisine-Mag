12746f8fe49cf483863036769c59aaa044d80b60
LuxionRob <aidaynhi8@gmail.com>
2023-12-26 13:26:18 +0700

    [Fix] fix cors header middleware

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['http://localhost:8000'],
 
     'allowed_headers' => ['*'],
 
@@ -30,5 +28,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
